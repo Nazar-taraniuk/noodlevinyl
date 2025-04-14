35da8e4181fe77f267b332d07a4bff2c451a73dc
Nazar-taraniuk <nazar.taraniuk@gmail.com>
2025-04-14 19:50:31 +0300

    lab 2

--- main/views.py
+++ main/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
 
-# Create your views here.
+def home_view(request):
+    pages = [
+        {'url': 'page1', 'title': 'Сторінка 1'},
+        {'url': 'page2', 'title': 'Сторінка 2'},
+        {'url': 'page3', 'title': 'Сторінка 3'},
+        {'url': 'page4', 'title': 'Сторінка 4'},
+    ]
+    return render(request, 'main/home.html', {'title': 'Головна', 'pages': pages})
+
+def page1_view(request):
+    return render(request, 'main/page.html', {'title': 'Сторінка 1'})
+
+def page2_view(request):
+    return render(request, 'main/page.html', {'title': 'Сторінка 2'})
+
+def page3_view(request):
+    return render(request, 'main/page.html', {'title': 'Сторінка 3'})
+
+def page4_view(request):
+    return render(request, 'main/page.html', {'title': 'Сторінка 4'})
